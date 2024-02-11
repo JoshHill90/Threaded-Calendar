c03e8eb9f7dfea8bb3f89899d471b5c012d7a8d3
JoshHill90 <joshua.j.hill90@gmail.com>
2024-02-11 13:35:16 -0500

    added logging to project
    added logging to project

--- calendar_project/calendar_app/views.py
+++ calendar_project/calendar_app/views.py
@@ -10,99 +10,154 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import json
 from .serializers import EventSerializer
-
+from logs.logging_config import logging
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+# Request to get calendar data
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def clandar(request, year, month):
-    last_Day = month_last_day(year, month)
-    request_month = int(month)
-    print(request_month)
-    request_year = int(year)
-    month0, month1, month2, cal, year_new, todays_date, cal_date = cal_gen(request_month, request_year)
-    month1 = month1.title()
-    month_number = list(calendar.month_name).index(month1)
-    month_number = int(month_number) 
-    next_year = year_new + 1
-    last_year = year_new - 1
-    
-    events_list = Event.objects.filter(date__range=[f"{year}-{month}-01", f"{year}-{month}-{last_Day[1]}"])
-    serialized_event_list = EventSerializer(events_list, many=True)
-    
-    data = {'year': year_new,
-        'month1': month1,
-        'month0':month0,
-        'month2' :month2,
-        'cal': cal,
-        'last_year': last_year,
-        'next_year': next_year,
-        'todays_date': todays_date.isoformat(),
-        'cal_date': cal_date.isoformat(),
-        'events': serialized_event_list.data
-        }
+    if request.method == 'GET':
+        try:
+            # int check!!
+            request_month = int(month)
+            request_year = int(year)
+            
+            # get's the last day of the month 
+            last_Day = month_last_day(year, month)
+            # cal gen takes the year and month as int and returns calendar and it's attrabutes 
+            month0, month1, month2, cal, year_new, todays_date, cal_date = cal_gen(request_month, request_year)
+            month1 = month1.title()
+            month_number = list(calendar.month_name).index(month1)
+            month_number = int(month_number) 
+            next_year = year_new + 1
+            last_year = year_new - 1
+            # pulls events for the givent mothn and year. 
+            events_list = Event.objects.filter(date__range=[f"{request_year}-{request_month}-01", f"{request_year}-{request_month}-{last_Day[1]}"])
+            serialized_event_list = EventSerializer(events_list, many=True)
+            
+            data = {'year': year_new,
+                'month1': month1,
+                'month0':month0,
+                'month2' :month2,
+                'cal': cal,
+                'last_year': last_year,
+                'next_year': next_year,
+                'todays_date': todays_date.isoformat(),
+                'cal_date': cal_date.isoformat(),
+                'events': serialized_event_list.data
+                }
 
-    return JsonResponse(data)
+            return JsonResponse(data)
+            # if the logic fails 
+        except Exception as e:
+            error_logged = f"Error in creating event {last_Day} \n {e} \n request infor {request}"
+            logging.error(error_logged)
+            return Response({'returned': error_logged})
+        
+    # not a post request
+    else:
+        resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        return Response(resp)  
 
+# create new event request
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def add_event(request):
     if request.method == 'POST':
-        event_object = Event.objects.all()
-        data = json.loads(request.body)
-        formData  = data.get('data')
+        try:
+            # pulls event model object
+            event_object = Event.objects.all()
+            # load data from form
+            data = json.loads(request.body)
+            formData  = data.get('data')
+            # exact calendar data object
+            subject = formData.get('subject')
+            date = formData.get('date')
+            start = formData.get('start')
+            end = formData.get('end')
+            event_type = formData.get('event_type')
+            details = formData.get('details')
+            # create event object
+            event_object.create(
+                subject = subject,
+                date = date,
+                start = start,
+                end = end,
+                event_type = event_type,
+                details = details
+            )
+            resp = {'returned':'success'}
+            return Response(resp)
         
-        subject = formData.get('subject')
-        date = formData.get('date')
-        start = formData.get('start')
-        end = formData.get('end')
-        event_type = formData.get('event_type')
-        details = formData.get('details')
+        # if the logic fails 
+        except Exception as e:
+            error_logged = f"Error in creating event {data} \n {e} \n request infor {request}"
+            logging.error(error_logged)
+            return Response({'returned': error_logged})
         
-        event_object.create(
-            subject = subject,
-            date = date,
-            start = start,
-            end = end,
-            event_type = event_type,
-            details = details
-        )
-        resp = {'returned':'success'}
-        return Response(resp)
+    # not a post request
     else:
         resp = {'returned':'wrong request method'}
-        return Response(resp)
-    
+        logging.error("Error - wrong request method")
+        return Response(resp)  
+        
+# Delete event request, with ID number   
 @api_view(['DELETE'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def delete_event(request, id):
-    event_to_delete = Event.objects.get(id=id)
-    event_to_delete.delete()
-    return HttpResponse({'response': 'success'})
+    if request.method == 'DELETE':
+        # delete logic
+        try: 
+            event_to_delete = Event.objects.get(id=id)
+            event_to_delete.delete()
+            return HttpResponse({'response': 'success'})
+        # if the logic fails
+        except Exception as e:
+            error_logged = f"Error in deleting event {event_to_delete} \n {e} \n request infor {request}"
+            logging.error(error_logged)
+            return Response({'returned': error_logged})
+    # not a delete request
+    else:
+        resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        
+        return Response(resp)    
 
-
+# request to update event by ID number
 @api_view(['PATCH'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def update_event(request, id):
+    # check if patch before tying to complete the logical process
     if request.method == 'PATCH':
-        data = json.loads(request.body)
-        formData  = data.get('data')
-        event_object = Event.objects.get(id=id)
-        event_object.subject = formData.get('subject')
-        event_object.date = formData.get('date')
-        event_object.start = formData.get('start')
-        event_object.end = formData.get('end')
-        event_object.event_type = formData.get('event_type')
-        event_object.details = formData.get('details')
-        event_object.save()
-        resp = {'returned':'success'}
-        return Response(resp)
+        try:
+            data = json.loads(request.body)
+            formData  = data.get('data')
+            event_object = Event.objects.get(id=id)
+            event_object.subject = formData.get('subject')
+            event_object.date = formData.get('date')
+            event_object.start = formData.get('start')
+            event_object.end = formData.get('end')
+            event_object.event_type = formData.get('event_type')
+            event_object.details = formData.get('details')
+            event_object.save()
+            resp = {'returned':'success'}
+            return Response(resp)
+        # if the logic fails
+        except Exception as e:
+            error_logged = f"Error in updating event {event_object} \n {e} \n request infor {request}"
+            logging.error(error_logged)
+            return Response({'returned': error_logged})
+    # not a patch request
     else:
         resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        
         return Response(resp)
--- calendar_project/user_auth/views.py
+++ calendar_project/user_auth/views.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 import json
+from logs.logging_config import logging
 ## auth imports
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
@@ -13,41 +14,84 @@ from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def login(request):
-
-    user = get_object_or_404(User, username=request.data['data']['username'])
-    if not user.check_password(request.data['data']['password']):
-        return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-    token, created  = Token.objects.get_or_create(user=user)
-
-    serializer = UserSerializer(instance=user)
-    return Response({'token': token.key, 'user': serializer.data})
+    if request.method == 'POST':
+        try:
+            user = get_object_or_404(User, username=request.data['data']['username'])
+            if not user.check_password(request.data['data']['password']):
+                return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
+            token, created  = Token.objects.get_or_create(user=user)
+            serializer = UserSerializer(instance=user)
+            return Response({'token': token.key, 'user': serializer.data})
+        # if the logic fails
+        except KeyError:
+            error_logged = f"Error: Required data not found in request: {request.data}"
+            logging.error(error_logged)
+            return Response({'error': error_logged}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            error_logged = f"Error: User does not exist for username: {request.data.get('username')}"
+            logging.error(error_logged)
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            error_logged = f"Error in login: {e}"
+            logging.error(error_logged)
+            return Response({'error': error_logged}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        
+        return Response(resp, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 def signup(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(username=request.data['username'])
-        # sets users' password as a hash password
-        user.set_password(request.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return Response({'token': token.key, 'user': serializer.data})
-    return Response({})
+    if request.method == 'POST':
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+
+                # sets users' password as a hash password
+                user.set_password(request.data['password'])
+                user.save()
+                token = Token.objects.create(user=user)
+                return Response({'token': token.key, 'user': serializer.data})
+            else:
+                return Response({'error': 'error not valid info'})
+        except KeyError:
+            error_logged = f"Error: Required data not found in request: {request.data}"
+            logging.error(error_logged)
+            return Response({'error': error_logged}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            error_logged = f"Error in signup process: {e}"
+            logging.error(error_logged)
+            return Response({'error': error_logged}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # not a patch request
+    else:
+        resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        return Response(resp, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def logout(request):
-
-    try:
-        token = Token.objects.get(key=request.auth.key)
-    except Token.DoesNotExist:
-        return Response({'error': 'Token does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-
-    token.delete()
-    
-    return Response({'message': 'Successfully logged out.'})
+    if request.method == 'POST':
+        try:
+            token = Token.objects.get(key=request.auth.key)
+            token.delete()
+            return Response({'message': 'Successfully logged out.'})
+        # if token does not exist
+        except Token.DoesNotExist:
+            logging.error(f"Token does not exist. Request info: {request}")
+            return Response({'error': 'Token does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        # if there was an error in the login logic or process
+        except Exception as e:
+            logging.error(f"Error in logging out: {e}")
+            return Response({'error': 'An error occurred while logging out.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # not a patch request
+    else:
+        resp = {'returned':'wrong request method'}
+        logging.error("Error - wrong request method")
+        return Response(resp)
 
 @api_view(['GET'])
 #check token and session id
